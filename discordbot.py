da2943304ae51be01d2369d4b265b8a7b737f67b
baker2st <60068205+baker2st@users.noreply.github.com>
2020-01-22 14:02:23 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,30 +3,22 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
-# 接続に必要なオブジェクトを生成
 client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-@client.event
-async def on_voice_state_update(member, before, after):
-    if member.guild.id == 615477382857621504:
-        stream_channel = client.get_channel(668677645545898003)
-        alert_channel = client.get_channel(668586484462387233)
-        stream = discord.utils.find(VoiceState.self_stream, stream_channel)
-        if stream:
-            msg = f'{member.name} が {after.channel.name} でキラー配信を開始しました！'
-            await alert_channel.send(msg)
-client.run(token)
-
-@bot.command()
-async def Okama(ctx):
-    await ctx.send('かまおかま！？')
-
-bot.run(token)
+ 
+@client.event 
+async def on_voice_state_update(before, after): 
+    if after.server.id == '監視するサーバーid': 
+        nowtime = datetime.datetime.utcnow() 
+        nowtime = nowtime + datetime.timedelta(hours=9) 
+        nowtime = nowtime.strftime("%m/%d-%H:%M") 
+        vcchannel = client.get_channel('通知させたいテキストチャンネルid') 
+ 
+        if(before.voice_channel is None): 
+            jointext=nowtime + "に　"+ after.name + "　が　"+ after.voice_channel.name + " に参加しました。" 
+            await client.send_message(vcchannel, jointext) 
+        elif(after.voice_channel is None): 
+            outtext=nowtime + "に　"+ before.name + "　が　"+ before.voice_channel.name + " から退出しました。" 
+            await client.send_message(vcchannel, outtext) 
+ 
+client.run(token) 
